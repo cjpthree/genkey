d58359c02347151fd6a0f47371636b2128bebbc1
cjp <cjpthree@126.com>
2018-12-10 15:36:07 +0800

    init

---
+++ src/main/java/com/jarxi/genkey/GenkeyApplication.java
@@ -0,0 +1,12 @@
+package com.jarxi.genkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenkeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenkeyApplication.class, args);
+    }
+}
